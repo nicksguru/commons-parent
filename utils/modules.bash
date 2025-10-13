3165c1ac097ef7841a3a0919af8f212b3f639e6a
Nick S. <job-offers@nicks.guru>
2025-10-13 22:16:12 +0300

    Upgraded dependencies.

---
+++ utils/modules.bash
@@ -0,0 +1,90 @@
+declare -a CURRENT_MODULE_VERSIONS=(
+    org.springframework.boot:spring-boot-starter-parent:3.5.6
+
+    org.apache.maven.plugins:maven-checkstyle-plugin:3.6.0
+    org.apache.maven.plugins:maven-surefire-plugin:3.5.4
+    org.apache.maven.plugins:maven-gpg-plugin:3.2.8
+    org.apache.maven.plugins:maven-javadoc-plugin:3.12.0
+    org.apache.maven.plugins:maven-source-plugin:3.2.1
+
+    com.github.ferstl:depgraph-maven-plugin:4.0.3
+    com.mysema.maven:apt-maven-plugin:1.1.3
+    org.jacoco:jacoco-maven-plugin:0.8.14
+    org.qunix:structure-maven-plugin:0.0.2
+    ch.acanda.maven:spring-banner-plugin:1.6.0
+
+    pw.krejci:jmh-maven-plugin:0.2.2
+    org.openjdk.jmh:jmh-core:1.37
+
+    org.fugerit.java:freemarker-maven-plugin:1.2.0
+    org.freemarker:freemarker:2.3.34
+
+    org.camunda.bpm.springboot:camunda-bpm-spring-boot-starter-webapp:7.23.0
+
+    io.cucumber:cucumber-bom:7.30.0
+    io.github.resilience4j:resilience4j-bom:2.3.0
+    org.junit:junit-bom:6.0.0
+    org.assertj:assertj-bom:3.27.3
+
+    org.projectlombok:lombok:1.18.42
+    org.mapstruct:mapstruct-processor:1.6.3
+    org.testcontainers:testcontainers:1.21.3
+    am.ik.yavi:yavi:0.16.0
+
+    org.springframework.cloud:spring-cloud-contract-wiremock:4.3.0
+    org.springframework.cloud:spring-cloud-dependencies:2025.0.0
+    io.awspring.cloud:spring-cloud-aws-dependencies:3.4.0
+    software.amazon.awssdk:bom:2.35.5
+
+    org.springframework.statemachine:spring-statemachine-autoconfigure:4.0.1
+    org.springframework.statemachine:spring-statemachine-data-jpa:4.0.1
+
+    org.togglz:togglz-spring-boot-starter:4.4.0
+    org.apache.groovy:groovy-jsr223:5.0.1
+    com.bucket4j:bucket4j_jdk17-core:8.15.0
+    org.redisson:redisson-spring-boot-starter:3.52.0
+    io.mongock:mongock-community-bom:5.5.1
+    io.freefair.okhttp-spring-boot:okhttp5-spring-boot-starter:3.5.3
+    com.gruelbox:transactionoutbox-spring:6.0.585
+    org.apache.activemq:activemq-client:6.1.7
+
+    commons-beanutils:commons-beanutils:1.11.0
+    commons-codec:commons-codec:1.19.0
+    commons-validator:commons-validator:1.10.0
+    commons-io:commons-io:2.20.0
+    org.apache.commons:commons-lang3:3.19.0
+    org.apache.commons:commons-collections4:4.5.0
+    org.apache.commons:commons-text:1.14.0
+
+    org.bouncycastle:bcprov-jdk18on:1.82
+    org.bouncycastle:bcpkix-jdk18on:1.82
+
+    com.google.guava:guava:33.5.0-jre
+    io.hypersistence:hypersistence-utils-hibernate-62:3.9.4
+    org.hibernate.orm:hibernate-spatial:6.6.18.Final
+    org.hashids:hashids:1.0.3
+    de.codecentric:spring-boot-admin-starter-server:3.5.5
+    org.springframework.security:spring-security-oauth2-client:6.4.7
+    com.nimbusds:nimbus-jose-jwt:10.5
+
+    org.springdoc:springdoc-openapi-starter-webmvc-api:2.8.6
+    org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6
+    io.swagger.core.v3:swagger-annotations:2.2.39
+
+    org.xerial.snappy:snappy-java:1.1.10.8
+    ru.odnoklassniki:one-nio:2.1.0
+    net.logstash.logback:logstash-logback-encoder:8.1
+    net.bytebuddy:byte-buddy:1.17.8
+    org.objenesis:objenesis:3.4
+    com.github.luben:zstd-jni:1.5.7-5
+
+    net.openhft:zero-allocation-hashing:0.27ea1
+    com.github.f4b6a3:uuid-creator:6.1.1
+    com.github.bastiaanjansen:otp-java:2.1.0
+    io.github.mysto:ff3:1.2.0
+
+    com.google.zxing:javase:3.5.3
+    com.github.auties00:qr-terminal:2.2
+    com.maxmind.geoip2:geoip2:4.4.0
+    com.googlecode.libphonenumber:libphonenumber:9.0.16
+)
